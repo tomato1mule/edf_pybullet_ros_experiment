58aa2b0b714a72756b83a810b293bfb172a836f1
Hyunwoo Ryu <tomato1mule@gmail.com>
2023-02-21 20:13:39 +0900

    visibility

--- dash_server_test.py
+++ dash_server_test.py
@@ -7,6 +7,7 @@ from edf.preprocess import Rescale, NormalizeColor, Downsample, PointJitter, Col
 
 from dash import Dash, html, dcc, Input, Output
 import dash_vtk
+import dash_daq
 import numpy as np
 import torch
 
@@ -28,28 +29,6 @@ best_pose = SE3(train_logs['best_neg_T'])
 sampled_poses= SE3(train_logs['sampled_Ts'])
 
 
-def PointCloudRepresentation(**kwargs):
-  return dash_vtk.GeometryRepresentation(
-      id=kwargs.get('id'),
-      colorMapPreset=kwargs.get('colorMapPreset'),
-      colorDataRange=kwargs.get('colorDataRange'),
-      property=kwargs.get('property'),
-      children=[
-      dash_vtk.PolyData(
-          points=kwargs.get('xyz'),
-          connectivity='points',
-          children=[
-          dash_vtk.PointData([
-              dash_vtk.DataArray(
-              registration='setScalars',
-              values={kwargs.get('scalars')}
-              )
-          ])
-          ],
-      )
-      ],
-  )
-
 def get_pcd_repr(id, points, colors):
     return dash_vtk.GeometryRepresentation(id=id, 
                                            children=[dash_vtk.PolyData(points=points.ravel(), 
@@ -60,15 +39,10 @@ def get_pcd_repr(id, points, colors):
                                                                                                                         values=colors.ravel() * 255)])],)],)
 
 
-
-
 scene_repr = get_pcd_repr(id = 'scene-pcd', points = scene_raw.points, colors=scene_raw.colors)
 grasp_repr = get_pcd_repr(id = 'grasp-pcd', points = grasp_raw.points, colors=grasp_raw.colors)
-vtk_view = dash_vtk.View(children=[scene_repr, grasp_repr], id="vtk-view1")
+vtk_view = dash_vtk.View(children=[scene_repr, grasp_repr], id="vtk-view1", background=[1., 1., 1.])
 
-
-grasp2_repr = get_pcd_repr(id = 'grasp2-pcd', points = grasp_raw.points, colors=grasp_raw.colors)
-vtk_view2 = dash_vtk.View(children=[grasp2_repr], id="vtk-view2")
 
 
 
@@ -80,39 +54,45 @@ server = app.server
 #     children=[html.Div(vtk_view, style={"height": "100%", "width": "100%"})],
 # )
 rows = []
-first_row = html.Div(children=[html.Div(children=[vtk_view], style={'padding': 10, 'flex': 1, "height": "30%", "width": "30%"}, id="vtk-view1-panel"),
-                               html.Div(children=[vtk_view2], style={'padding': 10, 'flex': 1, "height": "30%", "width": "30%"}, id="vtk-view2-panel"),
+first_row = html.Div(children=[html.Div(children=[html.Div(["Show Scene: ", dash_daq.BooleanSwitch(id='scene_visible_toggle', on=True),]),
+                                                  html.Div(["Show Gripper: ", dash_daq.BooleanSwitch(id='grasp_visible_toggle', on=True),])
+                                                  ], style={"height": "1000px", "width": "500px"}, id="left-panel"),
+
+                               html.Div(children=[vtk_view], style={"height": "1000px", "width": "1000px"}, id="vtk-view1-panel"),
+
                                html.Div([html.H6("Change the value in the text box to see callbacks in action!"),
                                          html.Div(["Input: ", dcc.Input(id='my-input', value='initial value', type='text')]),
                                          html.Br(),
-                                         html.Div(id='my-output'),] , id="input-panel"),
+                                         html.Div(id='my-output'),] , id="right-panel"),                            
                                ],
-                     style={'display': 'flex', 'flex-direction': 'row', "height": "calc(100vh - 16px)"})
+                     style={'display': 'flex', 'flex-direction': 'row', "height": "1200px", })
 rows.append(first_row)
 # second_row = html.Div(children=["Input: ", dcc.Input(id='my-input', value='initial value', type='text')])
 # rows.append(second_row)
-app.layout = html.Div(children=rows, style={'display': 'flex', 'flex-direction': 'column', "height": "calc(100vh - 16px)"}, id='main_panel')
+app.layout = html.Div(children=rows, style={'display': 'flex', 'flex-direction': 'column'}, id='main_panel')
 
 
 
 @app.callback(
-    # Output(component_id='my-output', component_property='children'),
-    Output("scene-pcd", "actor"),
+    Output(component_id='my-output', component_property='children'),
     Input(component_id='my-input', component_property='value')
 )
 def update_output_div(input_value):
-    print(type(input_value))
-    if input_value == 'invis':
-        return {"visibility": False}
-    else:
-        return {"visibility": True}
+    return f'Output: {input_value}'
 
+@app.callback(
+    Output(component_id="scene-pcd", component_property="actor"),
+    Input(component_id='scene_visible_toggle', component_property='on')
+)
+def update_output_div(input_value):
+    return {"visibility": input_value}
 
-
-
-
-
-
+@app.callback(
+    Output(component_id="grasp-pcd", component_property="actor"),
+    Input(component_id='grasp_visible_toggle', component_property='on')
+)
+def update_output_div(input_value):
+    return {"visibility": input_value}
 
 
 
