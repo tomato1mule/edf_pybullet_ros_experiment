4f5dab097a13f0dd4aa78dd0742a11bb28161b91
Hyunwoo Ryu <tomato1mule@gmail.com>
2023-04-25 16:39:25 +0900

    added argparse

--- bringup_edf_env_ros_eval.sh
+++ bringup_edf_env_ros_eval.sh
@@ -1,1 +1,6 @@
-roslaunch ros_edf ros_edf_env_unseen.launch
+target_obj_pose=random # random, upright, lying
+target_obj_name=random # train/mug0, test/mug1~10
+n_distractors=4
+use_support=true
+
+roslaunch ros_edf ros_edf_env.launch target_obj_pose:=$target_obj_pose target_obj_name:=$target_obj_name n_distractors:=$n_distractors use_support:=$use_support
